8f7ee4936ffef07661270926483dc3d070660a3e
Yangbee Thao <38472231+SirVanish@users.noreply.github.com>
2020-01-04 13:31:02 -0600

    Neural Nets added

---
+++ Assets/Scripts/NeuralNetwork/Intent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Intent
+{
+    public string intent { get; }
+
+    public Intent(string intent)
+    {
+        this.intent = intent;
+    }
+}
---
+++ Assets/Scripts/NeuralNetwork/Yui.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// check out:
+// https://github.com/gsssrao/UnityAndroidSpeechRecognition
+// https://cmusphinx.github.io/wiki/tutorialandroid/#using-pocketsphinx-android
+//
+// https://docs.unity3d.com/ScriptReference/AndroidJavaClass.html
+// https://docs.unity3d.com/Manual/AndroidAARPlugins.html
+
+class Yui : MonoBehaviour
+{
+    Net nn = new Net();
+
+    private void Start()
+    {
+
+        // create and load speech recognizer w/ default dictations
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
